07cd69cf47de158bf612c271411ba74c85633636
Wilson Ng <wilsonng234@gmail.com>
2024-01-31 15:30:26 +0800

    Create api for edge endpoints

---
+++ src/backend/edge/getWalkingPathWithinBoundingBox.tsx
@@ -0,0 +1,14 @@
+import api from '../api';
+
+const getWalkingPathWithinBoundingBox = async (
+  floorId: string,
+  boxCoordinates: string,
+) => {
+  const res = await api.get(
+    `/floors/${floorId}/nodes?boxCoordinates=${boxCoordinates}`,
+  );
+
+  return res.data;
+};
+
+export default getWalkingPathWithinBoundingBox;
