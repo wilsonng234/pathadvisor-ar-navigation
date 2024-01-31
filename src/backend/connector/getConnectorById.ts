1f2c10c2b093c13330a4844270a4b6ffdce6126c
Wilson Ng <wilsonng234@gmail.com>
2024-01-31 19:56:11 +0800

    Create api for connector endpoints

---
+++ src/backend/connector/getConnectorById.ts
@@ -0,0 +1,9 @@
+import api from '../api';
+
+const getConnectorById = async (connectorId: string) => {
+  const res = await api.get(`/connectors/${connectorId}`);
+
+  return res.data;
+};
+
+export default getConnectorById;
