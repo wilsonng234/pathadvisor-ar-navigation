4fa9c8ad4f1042ac4f084898e4931f7090cd02cd
Wilson Ng <wilsonng234@gmail.com>
2024-01-31 17:20:03 +0800

    Create api for search endpoints

---
+++ src/backend/search/getShortestPath.tsx
@@ -0,0 +1,24 @@
+import api from '../api';
+
+const getShortestPath = async (
+  fromId: string,
+  toId: string,
+  mode: string | null = null,
+  noStairCase: boolean | null = null,
+  noEscalator: boolean | null = null,
+  stepFreeAccess: boolean | null = null,
+  viaIds: string[] | null = null,
+) => {
+  const res = await api.get(
+    `/shortest-path?fromId=${fromId}&toId=${toId}` +
+      (mode ? `&mode=${mode}` : '') +
+      (noStairCase ? `&noStairCase=${noStairCase}` : '') +
+      (noEscalator ? `&noEscalator=${noEscalator}` : '') +
+      (stepFreeAccess ? `&stepFreeAccess=${stepFreeAccess}` : '') +
+      (viaIds ? viaIds.map(id => `&viaIds=${id}`).join('') : ''),
+  );
+
+  return res.data;
+};
+
+export default getShortestPath;
