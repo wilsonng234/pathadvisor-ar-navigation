633ba05c2d02ed0368acd9f57cbc38ab7d36cff2
Kiu <60544742+kiukiu328@users.noreply.github.com>
2024-02-02 19:41:10 +0800

    Rename to "getFloorById"

--- src/backend/floor/getFloorById.ts
+++ src/backend/floor/getFloorById.ts
@@ -1,9 +1,9 @@
 import api from '../api';
 
-const getFloor = async (floor:String) => {
+const getFloorById = async (floor:String) => {
   const res = await api.get(`/floors/${floor}`);
 
   return res.data;
 };
 
-export {getFloor};
+export {getFloorById};
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -2,7 +2,7 @@ export {getAllBuildings} from "./building/getAllBuildings";
 export {getConnectorById} from "./connector/getConnectorById";
 export {getWalkingPathWithinBoundingBox} from "./edge/getWalkingPathWithinBoundingBox";
 export {getAllFloors} from "./floor/getAllFloors";
-export {getFloor} from "./floor/getFloor";
+export {getFloorById} from "./floor/getFloorById";
 export {getNodeById} from "./node/getNodeById";
 export {getNodeByNearCoordinates} from "./node/getNodeByNearCoordinates";
 export {getNodesByName} from "./node/getNodesByName";
