2dd11d560c14f02b0e82f2b78b46acf2bb5c7b9b
Kiu <60544742+kiukiu328@users.noreply.github.com>
2024-02-13 18:44:40 +0800

    Added "Show map nodes"

--- src/frontend/MapView.tsx
+++ src/frontend/MapView.tsx
@@ -1,7 +1,7 @@
 
-import React, { createRef, useState, useEffect } from 'react';
+import React, { createRef, useState, useEffect, forwardRef, useRef } from 'react';
 import PropTypes from 'prop-types';
-import { Image, StyleSheet, View, Button, Dimensions, ImageBackground, ImageStyle, Text } from 'react-native';
+import { Image, StyleSheet, View, Button, Dimensions, ImageBackground, ImageStyle, Text, TouchableOpacity } from 'react-native';
 import { SearchBar } from '@rneui/themed';
 import * as api from '../backend/';
 import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
@@ -78,14 +78,17 @@ const MapView = () => {
 
   const [enableToSearchBar, setEnableToSearchBar] = useState(false);
   const [fromNode, setFromNode] = useState<any>(null);
-  const [path, setPath] = useState({});
+  const [path, setPath] = useState(null);
   const [floor, setFloor] = useState("G");
+  const floorViewRef = useRef<any>();
   // function to select "from" node
   const selectFromNode = (node: any) => {
-    setFloor(node.floorId);
+    floorViewRef.current?.setFloor(node.floorId);
+
     setEnableToSearchBar(true);
     setFromNode(node);
   }
+
   // function to select "to" node
   const selectToNode = (node: any) => {
     api.getShortestPath(fromNode?._id, node?._id).then((res) => {
@@ -105,39 +108,77 @@ const MapView = () => {
     <>
       <SearchLocation selectNode={selectFromNode} placeholder="Search for a location" />
       {enableToSearchBar && <SearchLocation selectNode={selectToNode} placeholder="Search to location" />}
-      <FloorView floor={floor} node={fromNode} path={path} />
+      <FloorView ref={floorViewRef} floor={floor} node={fromNode} path={path} />
     </>
   );
 };
 
 /**
- * 
+ * FloorView component only show the map|pin|path...
  * @param {string} floor floor id
  * @param node node of (from|to) for show the pin or zoom
  * @param zoomToPin zoom to the pin or not
- * @param {Map} path path to draw
+ * @param {Map} path path to draw ({"floorId":[[x,x],[x,x]...,[x,x]],..})
  */
-const FloorView = ({ floor, node, zoomToPin, path }) => {
+const FloorView = forwardRef(({ node, zoomToPin, path }: { node: any, zoomToPin: boolean, path: object }, ref) => {
   const zoomableViewRef = createRef<ReactNativeZoomableView>();
-
 
   const [mapSize, setMapSize] = useState({ height: 0, width: 0 });
   const [nodePosition, setNodePosition] = useState({ x: 0, y: 0 });
+  const [floor, setFloor] = useState("G");
+  const [nodes, setNodes] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [showPin, setShowPin] = useState(false);
+  const iconSize = 0.012;
+
 
   useEffect(() => {
+    api.getAllTags().then((res) => {
+      res = res["data"];
+      setTags(res);
+    });
+  }, []);
+
+
+  useEffect(() => {
+    if (ref && 'current' in ref) {
+      ref.current = {
+        setFloor: setFloor
+      }
+    }
     let startX = 0, startY = 0;
     api.getFloorById(floor).then((res) => {
       res = res["data"];
       startX = res.startX;
       startY = res.startY;
       setMapSize({ height: res.mapHeight, width: res.mapWidth });
+      let boxCoordinates = (startX) + "," + (startY) + "," + (startX + res.mapWidth) + "," + (startY + res.mapHeight);
+
+      api.getNodesWithinBoundingBox(floor, boxCoordinates).then((res) => {
+        res = res["data"];
+        res.map((node: any) => {
+          node["position"] = [node["centerCoordinates"][0] - startX, node["centerCoordinates"][1] - startY];
+        });
+        setNodes(res);
+      });
     });
-    api.getNodeById(node?._id).then((res) => {
+
+
+    node && api.getNodeById(node._id).then((res) => {
+      setShowPin(node.floorId === floor);
       res = res["data"];
       setNodePosition({ x: res["centerCoordinates"][0] - startX, y: res["centerCoordinates"][1] - startY });
-
     });
+
+
   }, [floor, node, path]);
+
+
+
+  const changeFloor = () => {
+    setFloor(floor === "G" ? "1" : "G");
+  }
+
   return (
 
     <ReactNativeZoomableView
@@ -150,38 +191,79 @@ const FloorView = ({ floor, node, zoomToPin, path }) => {
       contentHeight={mapSize.height}
       contentWidth={mapSize.width}
     >
-      <ImageBackground 
-      source={{
-        uri: 'https://pathadvisor.ust.hk/api/floors/' + floor + '/map-image'
-      }} 
-      style={{ height: mapSize.height, width: mapSize.width, justifyContent: 'center', alignItems: 'center' }}>
-        <Image 
-        style={[styles.pin, { display: node ? 'flex' : 'none', left: nodePosition.x, top: nodePosition.y }]} 
-        source={require('./assets/pin.png')} />
+      <ImageBackground
+        source={{
+          uri: 'https://pathadvisor.ust.hk/api/floors/' + floor + '/map-image'
+        }}
+        style={{ height: mapSize.height, width: mapSize.width, justifyContent: 'center', alignItems: 'center' }}>
+        <Image
+          style={[styles.pin, { display: showPin ? 'flex' : 'none', left: nodePosition.x, top: nodePosition.y }]}
+          source={require('./assets/pin.png')} />
 
         {/* if width&height match map size the app will crash. So scale it*/}
-        <View style={[{ 
-          width: mapSize.width / 10, 
-          height: mapSize.height / 10, 
-          margin: 'auto', 
-          transform: [{ scale: 10 }] }]}>
+        <View style={[{
+          width: mapSize.width / 10,
+          height: mapSize.height / 10,
+          margin: 'auto',
+          transform: [{ scale: 10 }]
+        }]}>
           <Svg viewBox={`0 0 ${mapSize.width} ${mapSize.height}`} height="100%" width="100%" >
             {path &&
               <Polyline points={path[floor]} stroke="red" strokeWidth="10" fill="none" />
             }
           </Svg>
         </View>
+
+        {
+          (tags.length > 0) && nodes && nodes.map((node: any, index: number) => {
+
+            let tag = node["tagIds"] ? tags.find((tag: any) => tag._id === node["tagIds"][0]) : null;
+            return (
+              <TouchableOpacity
+                onPress={changeFloor}
+                key={node._id}
+                style={{
+                  position: 'absolute',
+                  left: node["position"][0],
+                  top: node["position"][1],
+                }}>
+                {(tag && tag["imageUrl"]) ? (
+                  <Image
+                    style={{
+                      width: mapSize.width / (1 / iconSize),
+                      height: mapSize.width / (1 / iconSize)
+                    }}
+                    source={{ uri: tag["imageUrl"] }}
+                  />
+                ) : (
+                  <Text
+                    style={{
+                      fontSize: mapSize.width / (1 / (iconSize * 0.5)),
+                      color: 'black',
+                      left: -mapSize.width / (1 / (iconSize * 0.5)),
+                      top: -mapSize.width / (1 / (iconSize * 0.5)),
+                    }}>
+                    {node.name}
+                  </Text>
+                )}
+              </TouchableOpacity>);
+          })
+        }
+
+
       </ImageBackground>
+
     </ReactNativeZoomableView>
 
   )
-}
-FloorView.propTypes = {
-  floor: PropTypes.string.isRequired,
-  node: PropTypes.object,
-  zoomToPin: PropTypes.bool,
-  path: PropTypes.object
-};
+});
+
+// FloorView.propTypes = {
+//   node: PropTypes.object.isRequired,
+//   zoomToPin: PropTypes.bool.isRequired,
+//   path: PropTypes.object.isRequired
+// };
 
 
 export default MapView;
+export { FloorView };
