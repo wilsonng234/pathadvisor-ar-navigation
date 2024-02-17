3c8ebc44b5081a0503afc1edc44e5509780657ce
Wilson Ng <wilsonng234@gmail.com>
2024-02-17 18:11:51 +0800

    Refactor SearchLocationBar

--- src/frontend/components/SearchLocationBar.tsx
+++ src/frontend/components/SearchLocationBar.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { View, Text, Keyboard } from 'react-native';
+import { Text, Keyboard, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SearchBar } from '@rneui/themed';
 
 import * as api from '../../backend/';
 
-
 interface SearchLocationBarProps {
     placeholder: string;
     selectNode: (node: any) => void;
+}
+
+interface SearchResult {
+    name: string;
+    floorId: string;
 }
 
 /**
@@ -18,13 +22,20 @@ interface SearchLocationBarProps {
  * @returns 
  */
 const SearchLocationBar = ({ placeholder, selectNode }: SearchLocationBarProps) => {
-    const [searchText, setSearchText] = React.useState('');
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchText, setSearchText] = React.useState<string>('');
+    const [searchResults, setSearchResults] = useState<Array<SearchResult>>([]);
 
     const handleSearchTextChange = (s: string) => {
         setSearchText(s);
         api.getNodesByName(s).then((res) => {
-            setSearchResults(res.data);
+            setSearchResults(
+                res.data.map((item: any) => {
+                    return {
+                        name: item.name,
+                        floorId: item.floorId,
+                    }
+                })
+            )
         });
     }
 
@@ -43,25 +54,38 @@ const SearchLocationBar = ({ placeholder, selectNode }: SearchLocationBarProps) 
                 value={searchText} onChange={(e) => handleSearchTextChange(e.nativeEvent.text)}
                 showCancel={true} onCancel={handleSearchTextCancel} />
 
-            <View>
+            <>
                 {
                     // display search results dropdown
-                    searchResults.map((item: any, index: number) => (
+                    searchResults.map((item: any) => (
                         <TouchableOpacity
-                            style={{ padding: 7, borderBottomWidth: 1, borderColor: "grey" }}
-                            key={index}
+                            style={styles.searchResult}
                             onPress={() => {
-                                Keyboard.dismiss();
                                 selectNode(item);
                                 setSearchResults([]);
                                 setSearchText(item.name);
+
+                                Keyboard.dismiss();
                             }}>
-                            <Text style={{ color: "black", textAlign: "center" }}>{`${item.name} (${item.floorId})`}</Text>
+                            <Text style={styles.searchResultText}>{`${item.name} (${item.floorId})`}</Text>
                         </TouchableOpacity>
-                    ))}
-            </View>
+                    ))
+                }
+            </>
         </>
     );
 }
 
 export default SearchLocationBar
+
+const styles = StyleSheet.create({
+    searchResult: {
+        padding: 7,
+        borderBottomWidth: 1,
+        borderColor: "grey"
+    },
+    searchResultText: {
+        color: "black",
+        textAlign: "center"
+    }
+})
