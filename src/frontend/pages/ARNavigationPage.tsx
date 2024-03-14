8691834ddbd71249fdf11c0e14fbbf0644af50cc
Wilson Ng <wilsonng234@gmail.com>
2024-03-14 13:10:16 +0800

    Test UnityView

--- src/frontend/pages/ARNavigationPage.tsx
+++ src/frontend/pages/ARNavigationPage.tsx
@@ -1,16 +1,58 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import UnityView from '@azesmway/react-native-unity';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import ARTopNavigationBar from '../components/ar/ARTopNavigationBar';
 import ARBottomNavigationBar from '../components/ar/ARBottomNavigationBar';
 import ARNavigationCamera from '../components/ar/ARNavigationCamera';
+import { View } from "react-native";
+
+interface IMessage {
+    gameObject: string;
+    methodName: string;
+    message: string;
+}
+
+const Unity = () => {
+    const unityRef = useRef<UnityView>(null);
+
+    useEffect(() => {
+        if (unityRef?.current) {
+            const message: IMessage = {
+                gameObject: 'gameObject',
+                methodName: 'methodName',
+                message: 'message',
+            };
+            unityRef.current.postMessage(message.gameObject, message.methodName, message.message);
+        }
+    }, []);
+
+    return (
+        <View style={{ flex: 1 }}>
+            <UnityView
+                ref={unityRef}
+                style={{
+                    position: 'absolute',
+                    height: '100%',
+                    width: '100%',
+                    top: 1,
+                    bottom: 1
+                }}
+                onUnityMessage={(result) => {
+                    console.log('Message Here : ', result.nativeEvent.message)
+                }}
+            />
+        </View>
+    );
+};
 
 const ARNavigationPage = ({ navigation }) => {
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
-            <ARTopNavigationBar latitude={22.396427} longitude={114.109497} handleExitArNavigationPage={() => navigation.goBack()} />
+            <Unity></Unity>
+            {/* <ARTopNavigationBar latitude={22.396427} longitude={114.109497} handleExitArNavigationPage={() => navigation.goBack()} />
             <ARNavigationCamera />
-            <ARBottomNavigationBar />
+            <ARBottomNavigationBar /> */}
         </GestureHandlerRootView>
     );
 }
