41435767ff3fc7bad9638bcf36b78c44ee1f3754
Wilson Ng <wilsonng234@gmail.com>
2024-04-09 12:13:53 +0800

    Center NodeView

--- src/frontend/components/NodeView.tsx
+++ src/frontend/components/NodeView.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import FastImage from "react-native-fast-image";
-import { Text, View, ViewStyle } from "react-native";
+import { LayoutChangeEvent, Text, View, ViewStyle } from "react-native";
 import { UseQueryResult } from "@tanstack/react-query";
 
 import { LOGIC_MAP_TILE_WIDTH, LOGIC_MAP_TILE_HEIGHT, RENDER_MAP_TILE_HEIGHT, RENDER_MAP_TILE_WIDTH } from "./MapTilesBackground";
@@ -16,9 +16,17 @@ interface NodeViewProps {
     node: Node
 }
 
+interface NodeRenderSize {
+    [nodeId: number]: {
+        width: number
+        height: number
+    }
+}
+
 const NodeView = ({ currentFloorId, node }: NodeViewProps) => {
     const { data: floors, isLoading: isLoadingFloors }: UseQueryResult<FloorsDict> = useFloorsQuery()
     const { data: tags, isLoading: isLoadingTags }: UseQueryResult<TagsDict> = useTagsQuery()
+    const [nodeRenderSizes, setNodeRenderSizes] = useState<NodeRenderSize>({})
 
     if (!node.centerCoordinates)
         return null;
@@ -29,28 +37,42 @@ const NodeView = ({ currentFloorId, node }: NodeViewProps) => {
     // floors and tags are guaranteed to be loaded at this point
     const { tileStartX, tileStartY } = getMapTileStartCoordinates(floors![currentFloorId])
 
+    const handleLayoutChange = (e: LayoutChangeEvent) => {
+        setNodeRenderSizes({ ...nodeRenderSizes, [node._id]: { width: e.nativeEvent.layout.width, height: e.nativeEvent.layout.height } })
+    }
+
     return <View style={styles.container((node.centerCoordinates[0] - tileStartX) * (RENDER_MAP_TILE_WIDTH / LOGIC_MAP_TILE_WIDTH), (node.centerCoordinates[1] - tileStartY) * (RENDER_MAP_TILE_HEIGHT / LOGIC_MAP_TILE_HEIGHT))}>
         {
             // if node is a room, display the room name
             node.name?.toUpperCase().includes("ROOM") ?
-                <Text style={styles.text}>
+                <Text
+                    style={[styles.text, styles.animatedNode(nodeRenderSizes[node._id]?.width, nodeRenderSizes[node._id]?.height)]}
+                    onLayout={handleLayoutChange}
+                >
                     {node.name.replace("ROOM ", "")}
                 </Text> :
 
                 // else if node is a connector, display the connector image
                 node.connectorId && getNodeImageByConnectorId(node.connectorId) ?
                     <FastImage
+                        style={[styles.icon, styles.animatedNode(nodeRenderSizes[node._id]?.width, nodeRenderSizes[node._id]?.height)]}
+                        onLayout={handleLayoutChange}
                         source={getNodeImageByConnectorId(node.connectorId)}
-                        style={styles.icon} /> :
+                    /> :
 
                     // else if node contains tagIds, display the image of first tag
                     node.tagIds && node.tagIds.length > 0 && tags![node.tagIds[0]].imageUrl ?
                         <FastImage
+                            style={[styles.icon, styles.animatedNode(nodeRenderSizes[node._id]?.width, nodeRenderSizes[node._id]?.height)]}
+                            onLayout={handleLayoutChange}
                             source={{ uri: tags![node.tagIds[0]].imageUrl }}
-                            style={styles.icon} /> :
+                        /> :
 
                         // else display the name of the node
-                        <Text style={styles.text}>
+                        <Text
+                            style={[styles.text, styles.animatedNode(nodeRenderSizes[node._id]?.width, nodeRenderSizes[node._id]?.height)]}
+                            onLayout={handleLayoutChange}
+                        >
                             {node.name}
                         </Text>
         }
@@ -68,8 +90,13 @@ const styles = {
 
     text: {
         fontSize: 6,
-        color: 'black'
+        color: 'black',
     },
+
+    // center the node
+    animatedNode: (width: number, height: number) => ({
+        transform: [{ translateX: width ? -width / 2 : 0 }, { translateY: height ? -height / 2 : 0 }]
+    }),
 
     icon: {
         width: 10,
