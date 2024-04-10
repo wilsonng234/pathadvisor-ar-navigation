41791ebe5d7e8b53c92db2059230d2993763124c
MisakiTC <79361510+MisakiTC@users.noreply.github.com>
2024-04-10 18:45:07 +0800

    Bus Queue Linked API

--- src/frontend/screens/BusQueue.tsx
+++ src/frontend/screens/BusQueue.tsx
@@ -1,14 +1,72 @@
-import React from "react";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useEffect, useState } from "react";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import { Text, View } from "react-native";
 import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import { WebView } from 'react-native-webview';
+import axios from "axios";
 
 const BusQueueScreen = () => {
+    const [northPeople, setNorthPeople] = useState(0);
+    const [northTime, setNorthTime] = useState(0);
+    const [southPeople, setSouthPeople] = useState(0);
+    const [southTime, setSouthTime] = useState(0);
+    const getStat = () => {
+        axios.get('https://eek123.ust.hk/BusQueue/clientAPI/busqueue')
+            .then(
+                response => {
+                    setNorthPeople(response.data[response.data.length - 1]["north_count"]);
+                    setNorthTime(response.data[response.data.length - 1]["north_waiting"]);
+                    setSouthPeople(response.data[response.data.length - 1]["south_count"]);
+                    setSouthTime(response.data[response.data.length - 1]["south_waiting"]);
+                    console.log(response.data[response.data.length - 1]["south_count"])
+                }
+            )
+            .catch(e => console.log(e));
+    }
+
+    useEffect(() => {
+        getStat();
+    }, []);
+
     return (
         <ScrollView>
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={styles.container}>
+                    <Text style={styles.title}>North Bus Stop</Text>
+                    <View style={styles.infoContainer}>
+                        <SimpleLineIcons
+                            name="people"
+                            color="black"
+                            style={{ marginLeft: 8 }}   // move icon to the center
+                            size={20} />
+                        <Text style={styles.infoText}>{northPeople}</Text>
+                        <MaterialIcons
+                            name="access-time"
+                            color="black"
+                            style={{ marginLeft: 8 }}   // move icon to the center
+                            size={20} />
+                        <Text style={styles.infoText}>{Math.floor(northTime/60)}m{northTime%60}s</Text>
+                    </View>
+                </View>
                 <WebView style={styles.live} source={{ uri: 'https://pathadvisor.ust.hk/liveview/liveview-north.html' }} />
+                <View style={styles.container}>
+                    <Text style={styles.title}>South Bus Stop</Text>
+                    <View style={styles.infoContainer}>
+                        <SimpleLineIcons
+                            name="people"
+                            color="black"
+                            style={{ marginLeft: 8 }}   // move icon to the center
+                            size={20} />
+                        <Text style={styles.infoText}>{southPeople}</Text>
+                        <MaterialIcons
+                            name="access-time"
+                            color="black"
+                            style={{ marginLeft: 8 }}   // move icon to the center
+                            size={20} />
+                        <Text style={styles.infoText}>{Math.floor(southTime/60)}m{southTime%60}s</Text>
+                    </View>
+                </View>
                 <WebView style={styles.live} source={{ uri: 'https://pathadvisor.ust.hk/liveview/liveview-south.html' }} />
 
                 <View style={styles.reminderBox}>
@@ -26,9 +84,26 @@ export default BusQueueScreen;
 
 const styles = StyleSheet.create({
     container: {
-
+        borderWidth: 1,
+        borderColor: '#000',
+        borderRadius: 8,
+        padding: 16,
+        alignItems: 'center',
+        margin: 16,
     },
-
+    infoContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+    infoText: {
+        fontSize: 18,
+        marginHorizontal: 4,
+    },
     live: {
         flex: 1,
         width: Dimensions.get("screen").width,
