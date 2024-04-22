4872254a34d0b383c591ec2ada3ed652ac86549e
Wilson Ng <wilsonng234@gmail.com>
2024-04-22 11:37:18 +0800

    Type zustand store

--- src/frontend/hooks/store/useHomeStore.tsx
+++ src/frontend/hooks/store/useHomeStore.tsx
@@ -2,7 +2,16 @@ import { create } from 'zustand'
 
 import { BuildingsDict, FloorsDict, TagsDict } from '../../utils/storage_utils';
 
-const useHomeStore = create((set) => ({
+interface HomeState {
+    buildings: BuildingsDict | undefined,
+    setBuildings: (buildings: BuildingsDict) => void,
+    floors: FloorsDict | undefined,
+    setFloors: (floors: FloorsDict) => void,
+    tags: TagsDict | undefined,
+    setTags: (tags: TagsDict) => void,
+}
+
+const useHomeStore = create<HomeState>((set) => ({
     buildings: undefined,
     setBuildings: (buildings: BuildingsDict) => set({ buildings }),
     floors: undefined,
